3d9e48c8e1791db246c3c6b0ca8509cbd911ef48
henhenhentai <139239329+henhenhentai@users.noreply.github.com>
2025-06-04 19:20:49 +0800

    Add files via upload

---
+++ backend/yolo.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+
+img_path = 'dataset/tt100k_yolo/images/6071.jpg'
+yolo = YOLO('runs/detect/train/weights/best.pt') #看你要用best or last
+yolo(img_path ,show = True,save = True)
+
+results = yolo.predict(img_path)
+
+for result in results:
+    boxes = result.boxes.xyxy
+    scores = result.boxes.conf
+    classes = result.boxes.cls
+    class_names = [yolo.names[int(cls)] for cls in  classes]
+    for box ,  score , class_ , class_name in zip(boxes, scores, classes, class_names):
+        print (f" name: {class_name}, class: {class_}, confidence: {scores}, box: {boxes},")
