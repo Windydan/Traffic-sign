25773d587b7561c68ad05310754b594c18ba1b3a
henhenhentai <139239329+henhenhentai@users.noreply.github.com>
2025-06-10 17:37:59 +0800

    Update yolo.py

--- backend/yolo.py
+++ backend/yolo.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 
 img_path = 'dataset/tt100k_yolo/images/73.jpg'
-yolo = YOLO('runs/detect/train/weights/best.pt') #看你要用best or last
+yolo = YOLO('runs/detect/train/weights/last.pt') #看你要用best or last
 yolo(img_path ,show = True,save = True)
 
 results = yolo.predict(img_path)
